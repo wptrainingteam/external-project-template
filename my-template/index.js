40296efdac503f5a1ef460dc445435317d4c5623
Ryan Welcher <me@ryanwelcher.com>
2024-03-27 09:10:19 -0400

    Add all block related template files.

--- my-template/index.js
+++ my-template/index.js
@@ -6,26 +6,33 @@ const { join } = require("path");
 module.exports = {
   defaultValues: {
     transformer: (view) => {
-      console.log(view);
-      return { ...view, pluginDependencies: "gutenberg" };
+      const {
+        variantVars: { isInteractiveVariant },
+      } = view;
+      return {
+        ...view,
+        requiresAtLeast: isInteractiveVariant ? "6.5" : "6.1",
+      };
     },
     version: "1.0.0",
     namespace: "developer-blog",
+    render: "file:./render.php",
     customPackageJSON: {
       prettier: "@wordpress/prettier-config",
     },
   },
   variants: {
-    dynamic: {
-      render: "file:./render.php",
-    },
-    static: {},
+    dynamic: {},
     interactive: {
+      viewScriptModule: "file:./view.js",
+      customScripts: {
+        build: "wp-scripts build --experimental-modules",
+        start: "wp-scripts start --experimental-modules",
+      },
       supports: {
         interactive: true,
       },
     },
-    plugin: {},
   },
   pluginTemplatesPath: join(__dirname, "files/plugin"),
   blockTemplatesPath: join(__dirname, "files/block"),
